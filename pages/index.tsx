45c1f88dbc07be46536dc30fac1b7c89dcb199c6
royalventuresco <royalventuresco@gmail.com>
2025-05-28 16:05:33 -0400

    Create pages/index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#e6ede5",
+        color: "#2d3b2d",
+        fontFamily: "sans-serif",
+        fontSize: "1.5rem"
+      }}
+    >
+      Welcome to Royal Ventures & Co.
+    </div>
+  );
+}

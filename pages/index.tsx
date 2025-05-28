ab4cb94552657f7768c542e9c5f42f3085ff5c26
royalventuresco <royalventuresco@gmail.com>
2025-05-28 14:45:02 -0400

    Add files via upload
    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#e6ede5] text-[#2d3b2d] font-sans text-xl">
+      Welcome to Royal Ventures & Co.
+    </div>
+  );
+}

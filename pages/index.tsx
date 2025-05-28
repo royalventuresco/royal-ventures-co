e540a8eeb83149f49d47539ae389ad8d53bea7a9
royalventuresco <royalventuresco@gmail.com>
2025-05-28 15:26:21 -0400

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#e6ede5] text-[#2d3b2d] font-sans text-xl">
-      Welcome to Royal Ventures & Co.
-    </div>
-  );
-}
